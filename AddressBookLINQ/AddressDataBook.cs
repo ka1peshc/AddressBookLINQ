885d15c65d97c14632948c78dcef865eb0ac3471
ka1peshc <kalpesh111997@gmail.com>
2021-10-23 18:21:12 +0530

    Add: Create DataTable and add columns

--- AddressBookLINQ/AddressDataBook.cs
+++ AddressBookLINQ/AddressDataBook.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Linq;
 
 namespace AddressBookLINQ
 {
     class AddressDataBook
     {
+        public void AddData()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("FirstName");
+            table.Columns.Add("LastName");
+            table.Columns.Add("Address");
+            table.Columns.Add("City");
+            table.Columns.Add("State");
+            table.Columns.Add("Zip");
+            table.Columns.Add("PhoneNumber");
+            table.Columns.Add("Emails");
+
+        }
     }
 }
--- AddressBookLINQ/Program.cs
+++ AddressBookLINQ/Program.cs
@@ -7,6 +7,9 @@ namespace AddressBookLINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //UC 2
+            AddressDataBook adb = new AddressDataBook();
+            adb.AddData();
         }
     }
 }
